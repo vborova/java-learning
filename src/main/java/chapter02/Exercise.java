dbb9ad18578696d7c446cfd82e933012df324713
Veronica Borova <vborova@users.noreply.github.com>
2022-05-21 00:32:53 +0300

    Add JavaDoc comments

--- src/main/java/chapter02/Exercise.java
+++ src/main/java/chapter02/Exercise.java
@@ -1,6 +1,12 @@
 package chapter02;
 
 public class Exercise {
+    /**
+     * Calculates factorial by first approach.
+     *
+     * @param number source parameter
+     * @return factorial
+     */
     private static int factorial1(int number) {
         int result = 1;
 
@@ -11,13 +17,19 @@ public class Exercise {
         return result;
     }
 
+    /**
+     * Calculates factorial by second approach.
+     *
+     * @param number source parameter
+     * @return factorial
+     */
     private static int factorial2(int number) {
         if (number <= 1) {
             return 1;
         } else {
             return factorial2(number - 1) * number;
         }
-      }
+    }
 
     public static void main(String[] args) {
         System.out.printf("Factorial (variant 1): %d\n", factorial1(5));
