a09f7cd298528539bd989320f5f7ede3540262e5
Veronica Borova <vborova@users.noreply.github.com>
2022-06-18 00:16:31 +0300

    Change Chapter 4 exercise

--- src/main/java/chapter04/Exercise.java
+++ src/main/java/chapter04/Exercise.java
@@ -1,33 +1,53 @@
 package chapter04;
 
+import java.util.stream.IntStream;
+
 public class Exercise {
     private static int whileSum(int value1, int value2) {
         int result = 0;
+
         int i = value1;
         while (i <= value2) {
-            result = result + i;
+            result += i;
             i++;
         }
+
         return result;
     }
 
     private static int doWhileSum(int value1, int value2) {
         int result = 0;
         int i = value1;
+
         do {
-            result = result + i;
+            result += i;
             i++;
         } while (i <= value2);
+
         return result;
     }
 
     private static int forSum(int value1, int value2) {
         int result = 0;
+
         for (int i = value1; i <= value2; i++) {
-            result = result + i;
+            result += i;
         }
+
         return result;
     }
+
+    private static int forEachSum(int value1, int value2) {
+        int result = 0;
+        int[] values = IntStream.rangeClosed(value1, value2).toArray();
+
+        for (int value : values) {
+            result += value;
+        }
+
+        return result;
+    }
+
 
     public static void main(String[] args) {
         if (args.length < 3) {
@@ -40,9 +60,9 @@ public class Exercise {
             switch (functionType) {
                 case "0" -> System.out.printf("whileSum: %d\n", whileSum(value1, value2));
                 case "1" -> System.out.printf("doWhileSum: %d\n", doWhileSum(value1, value2));
-                default -> System.out.printf("forSum: %d\n", forSum(value1, value2));
+                case "2" -> System.out.printf("forSum: %d\n", forSum(value1, value2));
+                default -> System.out.printf("forEachSum: %d\n", forEachSum(value1, value2));
             }
         }
     }
-
 }
